70ab9d481f97ac5ad573bc4f57f64c140e599a62
Abdul Munim <abdulmunim2005@gmail.com>
2024-08-04 19:41:09 +0500

    Fixed the 7h in the bar

--- src/features/dashboard/components/BarChart.jsx
+++ src/features/dashboard/components/BarChart.jsx
@@ -53,20 +53,26 @@ function BarChart({
     },
   };
 
-  const getDateRangeLabels = (startDate, endDate) => {
+  const getDateRangeLabels = (startDate, endDate, interval) => {
     const result = [];
     const currentDate = new Date(startDate);
 
     if (
+      interval === "12h" ||
       startDate.toISOString().split("T")[0] ===
       endDate.toISOString().split("T")[0]
     ) {
-      for (let i = 0; i < 24; i++) {
+      // If the interval is "12h" or the start and end dates are the same, create hourly labels
+      const hoursInterval = interval === "12h" ? 1 : 2; // 1-hour intervals for 12h and 2-hour intervals for the same date
+      const hoursToShow = interval === "12h" ? 12 : 24;
+
+      for (let i = 0; i < hoursToShow; i++) {
         const date = new Date(startDate);
-        date.setHours(i);
+        date.setHours(date.getHours() - (hoursToShow - 1 - i) * hoursInterval);
         result.push(date.toISOString());
       }
     } else {
+      // Create daily labels
       while (currentDate <= endDate) {
         result.push(new Date(currentDate).toISOString().split("T")[0]);
         currentDate.setDate(currentDate.getDate() + 1);
@@ -87,18 +93,20 @@ function BarChart({
     return `${hours}:${minutesStr} ${ampm}`;
   };
 
-  const labels = getDateRangeLabels(dateRange.startDate, dateRange.endDate).map(
-    (date) => {
-      if (
-        interval === "12h" ||
-        dateRange.startDate.toISOString().split("T")[0] ===
-          dateRange.endDate.toISOString().split("T")[0]
-      ) {
-        return formatTime(date);
-      }
-      return date;
+  const labels = getDateRangeLabels(
+    dateRange.startDate,
+    dateRange.endDate,
+    interval
+  ).map((date) => {
+    if (
+      interval === "12h" ||
+      dateRange.startDate.toISOString().split("T")[0] ===
+      dateRange.endDate.toISOString().split("T")[0]
+    ) {
+      return formatTime(date); // Format time as human-readable
     }
-  );
+    return date;
+  });
 
   useEffect(() => {
     if (data && data.length > 0) {
@@ -107,11 +115,14 @@ function BarChart({
         if (
           interval === "12h" ||
           dateRange.startDate.toISOString().split("T")[0] ===
-            dateRange.endDate.toISOString().split("T")[0]
+          dateRange.endDate.toISOString().split("T")[0]
         ) {
           dayData = data.filter(
             (item) =>
-              new Date(item.updatedAt).getHours() === new Date(date).getHours()
+              new Date(item.updatedAt).getHours() ===
+              new Date(date).getHours() &&
+              new Date(item.updatedAt).toISOString().split("T")[0] ===
+              new Date(date).toISOString().split("T")[0]
           );
         } else {
           dayData = data.filter(
@@ -159,12 +170,12 @@ function BarChart({
 
   const onChange = (e) => {
     setInterval(e.target.value);
-    if (e.target.value !== "12h" && e.target.value === "15d") {
+    if (e.target.value === "15d") {
       setDateRange({
         startDate: new Date(new Date().setDate(new Date().getDate() - 15)),
         endDate: new Date(),
       });
-    } else if (e.target.value !== "12h" && e.target.value === "7d") {
+    } else if (e.target.value === "7d") {
       setDateRange({
         startDate: new Date(new Date().setDate(new Date().getDate() - 7)),
         endDate: new Date(),
