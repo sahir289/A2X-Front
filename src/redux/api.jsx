2420aefaf9142e60b07aa027539d9ea9eb17e1d2
mhammad100 <hammadmansha100@gmail.com>
2024-09-26 16:26:22 +0500

    added increased pagination if getting merchants

--- src/redux/api.jsx
+++ src/redux/api.jsx
@@ -20,6 +20,11 @@ const apiConfig = (flag = false) => {
 
 export const getApi = async (url, params) => {
   try {
+    // this is the temporary solution for getting all merchants in dropdowns.
+    // will improves this in future.
+    if (url.includes("getall-merchant") && !params.pageSize) {
+      params.pageSize = 1000;
+    }
     const response = await axios.get(`${endPoint}${url}`, {
       params: params,
       ...apiConfig(),
