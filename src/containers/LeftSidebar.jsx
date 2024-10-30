d020d91faa358e5b32a793d8c3a599f90d6b7662
Shadow <shadow77278837@gmail.com>
2024-10-30 15:14:44 +0400

    New development: Bank Report Download

--- src/containers/LeftSidebar.jsx
+++ src/containers/LeftSidebar.jsx
@@ -48,9 +48,9 @@ function LeftSidebar() {
 
 
   return (
-    <div className="drawer-side  z-30  ">
+    <div className="drawer-side z-30  ">
       <label htmlFor="left-sidebar-drawer" className="drawer-overlay"></label>
-      <ul className="menu  pt-2 w-60 bg-base-100 min-h-full   text-base-content">
+      <ul className="menu pt-2 w-30 bg-base-100 min-h-full text-base-content">
         <button className="btn btn-ghost bg-base-300  btn-circle z-50 top-0 right-0 mt-4 mr-2 absolute lg:hidden" onClick={() => close()}>
           <XMarkIcon className="h-5 inline-block w-5" />
         </button>
--- src/containers/PageContent.jsx
+++ src/containers/PageContent.jsx
@@ -22,7 +22,7 @@ function PageContent() {
   return (
     <div className="drawer-content flex flex-col ">
       <Header />
-      <main className="flex-1 overflow-y-auto md:pt-4 pt-4 px-6 bg-base-200" ref={mainContentRef}>
+      <main className="flex-1 overflow-y-auto md:pt-4 pt-3 px-3 bg-base-200" ref={mainContentRef}>
         <Suspense fallback={<SuspenseContent />}>
           <Routes>
             {
