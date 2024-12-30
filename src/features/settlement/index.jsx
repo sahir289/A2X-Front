34449779e957ee209daee9398beebd0918197f7f
psahir <psahir289@gmail.com>
2024-12-30 15:51:57 +0400

    bug: resolved getting settlements after approvinf

--- src/features/settlement/index.jsx
+++ src/features/settlement/index.jsx
@@ -1,18 +1,31 @@
 import { PlusOutlined, RedoOutlined } from "@ant-design/icons";
-import { Button, Form, Input, Modal, notification, Pagination, Select, Checkbox } from 'antd';
-import { useContext, useEffect, useRef, useState } from "react";
-import { useSelector } from "react-redux";
-import { getApi, postApi, putApi } from "../../redux/api";
-import { getQueryFromObject, reasonOptions, RequiredRule } from "../../utils/utils";
-import TableComponent, { methodOptions } from './components/Table';
-import { useNavigate } from "react-router-dom";
-import { NotificationContainer, NotificationManager } from 'react-notifications';
-import { PermissionContext } from "../../components/AuthLayout/AuthLayout";
+import {
+  Button,
+  Checkbox,
+  Form,
+  Input,
+  Modal,
+  Pagination,
+  Select,
+  notification,
+} from "antd";
 import axios from "axios";
-
+import { useContext, useEffect, useRef, useState } from "react";
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+import { useNavigate } from "react-router-dom";
+import { PermissionContext } from "../../components/AuthLayout/AuthLayout";
+import { getApi, postApi, putApi } from "../../redux/api";
+import {
+  RequiredRule,
+  getQueryFromObject,
+  reasonOptions,
+} from "../../utils/utils";
+import TableComponent, { methodOptions } from "./components/Table";
 
 export default function Settlement() {
-
   const [form] = Form.useForm();
   const [api, notificationContext] = notification.useNotification();
   const [modal, contextHolder] = Modal.useModal();
@@ -26,27 +39,29 @@ export default function Settlement() {
   const [settlements, setSettlements] = useState({
     data: [],
     total: 0,
-  })
-  const userData = useContext(PermissionContext)
+  });
+  const userData = useContext(PermissionContext);
   const [filters, setFilters] = useState({
-    code: userData?.code?.length ? userData?.code : null
-
+    code: userData?.code?.length ? userData?.code : null,
   });
   const [pagination, setPagination] = useState({
     page: 1,
     take: 20,
-  })
+  });
   const { data, total } = settlements;
   // const merchants = useSelector(state => state?.merchant?.data);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [merchantOptions, setMerchantOptions] = useState([]);
 
   useEffect(() => {
-    handleGetSettlements({
-      ...pagination,
-      ...filters,
-    }, false);
-  }, [pagination, filters])
+    handleGetSettlements(
+      {
+        ...pagination,
+        ...filters,
+      },
+      false
+    );
+  }, [pagination, filters]);
 
   useEffect(() => {
     let isMounted = true; // To avoid state updates on unmounted components
@@ -81,7 +96,8 @@ export default function Settlement() {
             ?.filter(
               (merchant) =>
                 !merchant.is_deleted &&
-                (!userData?.code?.length || userData?.code?.includes(merchant?.code))
+                (!userData?.code?.length ||
+                  userData?.code?.includes(merchant?.code))
             )
             .map((merchant) => ({
               label: merchant.code,
@@ -109,7 +125,7 @@ export default function Settlement() {
       getSettlementList({
         ...queryObj,
         // code: queryObj?.code || String(userData?.code[0]),
-        code: queryObj?.code || ""
+        code: queryObj?.code || "",
       });
       return;
     }
@@ -118,28 +134,29 @@ export default function Settlement() {
       getSettlementList({
         ...queryObj,
         // code: queryObj?.code || String(userData?.code[0]),
-        code: queryObj?.code || ""
-
+        code: queryObj?.code || "",
       });
     }, 1500);
-  }
+  };
 
   const getSettlementList = async (queryObj) => {
     const query = getQueryFromObject(queryObj);
     setIsLoading(true);
-    const res = await getApi(`/getall-settlement${query}&includeSubMerchant=${includeSubMerchant}`);
+    const res = await getApi(
+      `/getall-settlement${query}&includeSubMerchant=${includeSubMerchant}`
+    );
     setIsLoading(false);
     if (res?.error?.error?.response?.status === 401) {
       NotificationManager.error(res?.error?.message, 401);
       localStorage.clear();
-      navigate('/')
+      navigate("/");
     }
     const data = res?.data?.data;
     setSettlements({
       data: data?.data || [],
       total: data?.totalRecords || 0,
-    })
-  }
+    });
+  };
 
   const handleToggleModal = () => {
     setOpen(!open);
@@ -150,19 +167,19 @@ export default function Settlement() {
     setPagination({
       page,
       take: pageSize,
-    })
-  }
+    });
+  };
 
   const handleShowTotal = (total, range) => {
     if (!total) {
       return;
     }
     return (
-      <p className='text-base'>
+      <p className="text-base">
         {range[0]}-{range[1]} of {total} items
       </p>
-    )
-  }
+    );
+  };
 
   // Function to validate IFSC code using an API
   const validateIfscCode = async (ifsc) => {
@@ -181,7 +198,7 @@ export default function Settlement() {
     if (data?.method === "BANK") {
       const ifscValidation = await validateIfscCode(data?.ifsc);
       if (!ifscValidation) {
-        setAddLoading(false)
+        setAddLoading(false);
         api.error({
           message: "Invalid IFSC Code",
           description: "Please enter a valid IFSC code.",
@@ -190,7 +207,7 @@ export default function Settlement() {
       }
     }
 
-    const res = await postApi("/create-settlement", data)
+    const res = await postApi("/create-settlement", data);
     if (res?.error) {
       api.error({ description: res.error.message });
       return;
@@ -199,14 +216,14 @@ export default function Settlement() {
     handleToggleModal();
     getSettlementList();
     handleResetSearchFields();
-  }
+  };
 
   const onFilterChange = async (name, value) => {
     setFilters({
       ...filters,
       [name]: value,
-    })
-  }
+    });
+  };
 
   const updateSettlementStatus = (data) => {
     if (data.reset) {
@@ -215,9 +232,9 @@ export default function Settlement() {
 
     setEditSettlement({
       ...data.record,
-      key: data.key
+      key: data.key,
     });
-  }
+  };
 
   const handleResetSettlement = async (id) => {
     const isReset = await modal.confirm({
@@ -227,11 +244,14 @@ export default function Settlement() {
     });
 
     if (isReset) {
-      handelUpdateSettlement({
-        status: "INITIATED"
-      }, id);
+      handelUpdateSettlement(
+        {
+          status: "INITIATED",
+        },
+        id
+      );
     }
-  }
+  };
 
   const handelUpdateSettlement = async (data, id) => {
     const settlementId = editSettlement?.id || id;
@@ -240,8 +260,8 @@ export default function Settlement() {
     }
     if (editSettlement?.key == "approve" && editSettlement?.method !== "BANK") {
       data = {
-        refrence_id: ' '
-      }
+        refrence_id: " ",
+      };
     }
     setIsLoading(true);
     const res = await putApi(`/update-settlement/${settlementId}`, data);
@@ -250,48 +270,66 @@ export default function Settlement() {
       return;
     }
     setEditSettlement(null);
+    handleResetSearchFields();
     handleGetSettlements();
-  }
+  };
 
   const labelCol = { span: 6 };
   //reset search fields
   const handleResetSearchFields = () => {
-    setFilters({})
-  }
+    setFilters({});
+  };
 
   return (
-    <section className=''>
+    <section className="">
       {contextHolder}
       {notificationContext}
-      <div className='bg-white rounded-[8px] p-[8px]'>
-        <div className='flex justify-between mb-[10px] max-[500px]:flex-col max-[500px]:gap-[10px]'>
-          <p className='text-lg font-medium p-[5px]'>Settlement List</p>
-          <div className='flex items-start gap-4 max-[500px]:justify-end'>
-            <div className='flex flex-col items-start'>
+      <div className="bg-white rounded-[8px] p-[8px]">
+        <div className="flex justify-between mb-[10px] max-[500px]:flex-col max-[500px]:gap-[10px]">
+          <p className="text-lg font-medium p-[5px]">Settlement List</p>
+          <div className="flex items-start gap-4 max-[500px]:justify-end">
+            <div className="flex flex-col items-start">
               <Button
                 icon={<PlusOutlined />}
-                type='primary'
+                type="primary"
                 onClick={handleToggleModal}
               >
                 New Settlement
               </Button>
-              <Button className='mt-2 w-full' onClick={handleResetSearchFields}>Reset</Button>
+              <Button className="mt-2 w-full" onClick={handleResetSearchFields}>
+                Reset
+              </Button>
             </div>
-            <Button type="text" className='rounded-full h-[40px] w-[40px] p-[0px]' onClick={() => handleGetSettlements({ ...pagination, ...filters })}>
+            <Button
+              type="text"
+              className="rounded-full h-[40px] w-[40px] p-[0px]"
+              onClick={() =>
+                handleGetSettlements({ ...pagination, ...filters })
+              }
+            >
               <RedoOutlined size={24} className="rotate-[-90deg]" />
             </Button>
           </div>
         </div>
-        <div className="flex" style={{justifySelf: "end", marginRight: "40px"}}>
-          {(userData.role === "ADMIN" || userData.role === "TRANSACTIONS" || userData.role === "OPERATIONS") && <Checkbox
-            onClick={() => {
-              setIncludeSubMerchant((prevState) => !prevState);
-            }}
-          >
-            <span style={{ color: "cornflowerblue" }}>Include Sub Merchant</span>
-          </Checkbox>}
+        <div
+          className="flex"
+          style={{ justifySelf: "end", marginRight: "40px" }}
+        >
+          {(userData.role === "ADMIN" ||
+            userData.role === "TRANSACTIONS" ||
+            userData.role === "OPERATIONS") && (
+            <Checkbox
+              onClick={() => {
+                setIncludeSubMerchant((prevState) => !prevState);
+              }}
+            >
+              <span style={{ color: "cornflowerblue" }}>
+                Include Sub Merchant
+              </span>
+            </Checkbox>
+          )}
         </div>
-        <div className='overflow-x-auto'>
+        <div className="overflow-x-auto">
           <TableComponent
             loading={isLoading}
             data={data}
@@ -302,7 +340,7 @@ export default function Settlement() {
             userData={userData}
           />
         </div>
-        <div className='flex justify-end mt-[10px]'>
+        <div className="flex justify-end mt-[10px]">
           <Pagination
             total={total}
             pageSize={pagination.take}
@@ -323,107 +361,94 @@ export default function Settlement() {
         destroyOnClose
       >
         <Form layout="vertical" onFinish={handelUpdateSettlement}>
-          {
-            editSettlement?.key == "approve" ?
-              editSettlement?.method == "BANK" ?
-                <Form.Item
-                  name="refrence_id"
-                  label="UTR Number"
-                  rules={[
-                    { RequiredRule }
-                  ]}
-                >
-                  <Input size="large" />
-                </Form.Item> : null :
-              <Form.Item name="rejected_reason" label="Reason" rules={RequiredRule}>
-                <Select
-                  options={reasonOptions}
-                />
+          {editSettlement?.key == "approve" ? (
+            editSettlement?.method == "BANK" ? (
+              <Form.Item
+                name="refrence_id"
+                label="UTR Number"
+                rules={[{ RequiredRule }]}
+              >
+                <Input size="large" />
               </Form.Item>
-          }
-          {
-            editSettlement?.key == "approve" &&
-              editSettlement?.method !== "BANK" ?
-              <div>
-                <h5> Are you sure to approve? </h5>
-              </div>
-              : null
-          }
-          <div className="flex justify-end">
-            <Button
-              type="primary"
-              htmlType="submit"
-              loading={isLoading}
+            ) : null
+          ) : (
+            <Form.Item
+              name="rejected_reason"
+              label="Reason"
+              rules={RequiredRule}
             >
-              {
-                editSettlement?.key == "approve" ? "Approve" : "Reject"
-              }
+              <Select options={reasonOptions} />
+            </Form.Item>
+          )}
+          {editSettlement?.key == "approve" &&
+          editSettlement?.method !== "BANK" ? (
+            <div>
+              <h5> Are you sure to approve? </h5>
+            </div>
+          ) : null}
+          <div className="flex justify-end">
+            <Button type="primary" htmlType="submit" loading={isLoading}>
+              {editSettlement?.key == "approve" ? "Approve" : "Reject"}
             </Button>
           </div>
         </Form>
       </Modal>
 
-      <Modal title="Add Settlement" onCancel={handleToggleModal} open={open} footer={false}>
+      <Modal
+        title="Add Settlement"
+        onCancel={handleToggleModal}
+        open={open}
+        footer={false}
+      >
         <Form
           form={form}
-          className='pt-[10px]'
-          labelAlign='left'
+          className="pt-[10px]"
+          labelAlign="left"
           labelCol={labelCol}
           onFinish={handleSubmit}
         >
-          <Form.Item
-            name="code"
-            label="Merchant"
-            rules={RequiredRule}
-          >
-            <Select
-              showSearch
-              options={merchantOptions}
-            />
+          <Form.Item name="code" label="Merchant" rules={RequiredRule}>
+            <Select showSearch options={merchantOptions} />
           </Form.Item>
-          <Form.Item
-            name="amount"
-            label="Amount"
-            rules={RequiredRule}
-          >
+          <Form.Item name="amount" label="Amount" rules={RequiredRule}>
             <Input type="number" addonAfter="₹" />
           </Form.Item>
-          <Form.Item
-            name="method"
-            label="Method"
-            rules={RequiredRule}
-          >
-            <Select
-              options={methodOptions}
-            />
+          <Form.Item name="method" label="Method" rules={RequiredRule}>
+            <Select options={methodOptions} />
           </Form.Item>
-          {
-            method === "BANK" &&
+          {method === "BANK" && (
             <>
               <Form.Item name="acc_name" label="Name" rules={RequiredRule}>
                 <Input />
               </Form.Item>
-              <Form.Item name="acc_no" label="Bank Details" rules={RequiredRule}>
+              <Form.Item
+                name="acc_no"
+                label="Bank Details"
+                rules={RequiredRule}
+              >
                 <Input />
               </Form.Item>
               <Form.Item name="ifsc" label="IFSC" rules={RequiredRule}>
                 <Input />
               </Form.Item>
             </>
-          }
-          {
-            method === "CRYPTO" &&
+          )}
+          {method === "CRYPTO" && (
             <>
               <Form.Item name="wallet" label="Wallet" rules={RequiredRule}>
                 <Input />
               </Form.Item>
-              <Form.Item name="wallet_address" label="Wallet Address" rules={RequiredRule}>
+              <Form.Item
+                name="wallet_address"
+                label="Wallet Address"
+                rules={RequiredRule}
+              >
                 <Input />
               </Form.Item>
             </>
-          }
-          <div className='flex justify-end'>
-            <Button type='primary' loading={addLoading} htmlType='submit'>
+          )}
+          <div className="flex justify-end">
+            <Button type="primary" loading={addLoading} htmlType="submit">
               Save
             </Button>
           </div>
@@ -433,4 +458,3 @@ export default function Settlement() {
     </section>
   );
 }
-
