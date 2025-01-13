bcde9b7add31d2f18215f0c26439a8073c879705
sudhakartajalfahidi <sudhakar.tajalfahidi@gmail.com>
2025-01-13 12:09:44 +0400

    dropdown payout

--- src/features/withdraw/index.jsx
+++ src/features/withdraw/index.jsx
@@ -23,6 +23,7 @@ import {
   RequiredRule,
   getQueryFromObject,
   reasonOptions,
+  withdrawlMethods,
 } from "../../utils/utils";
 import Table from "./components/Table";
 
@@ -140,11 +141,7 @@ const Withdraw = ({ type }) => {
     userData?.role === "ADMIN" ||
     userData?.role === "TRANSACTIONS" ||
     userData?.role === "OPERATIONS"
-      ? [
-          { value: "manual", label: "Manual", key: "manual" },
-          { value: "eko", label: "Eko", key: "eko" },
-          //  { value: "blazepe", label: "BlazePe", key: "blazepe" },
-        ]
+      ? withdrawlMethods
       : [{ value: "manual", label: "Manual", key: "manual" }];
 
   useEffect(() => {
@@ -300,6 +297,7 @@ const Withdraw = ({ type }) => {
           status: "REJECTED",
           rejected_reason: "Invalid Bank Details"
         },
+
         id
       );
     }
