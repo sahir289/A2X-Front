72474a686ecc532d4fa980ed741bb88d0aaedb83
aryan11814 <aryan181431@gmail.com>
2025-02-07 18:20:19 +0530

    some changes

--- src/pages/protected/reports/VendorPayout.jsx
+++ src/pages/protected/reports/VendorPayout.jsx
@@ -2,32 +2,16 @@ import React, { useContext, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import VendorPayoutsComponent from '../../../components/reports/PayoutVendor';
 import { setPageTitle } from '../../../redux/slice/headerSlice';
-<<<<<<< HEAD
 
 const VendorPayout = () => {
   const dispatch = useDispatch()
-=======
-import VendorPayoutComponent from '../../../components/reports/VendorPayoutComponent';
-import { PermissionContext } from '../../../components/AuthLayout/AuthLayout';
-
-const VendorPayout = () => {
-  const dispatch = useDispatch()
-
-  const AllowedRoles = ["ADMIN","TRANSACTIONS","VENDOR","VENDOR_OPERATIONS","OPERATIONS"]
-  const context = useContext(PermissionContext)
-
->>>>>>> a3e5167482190782a5fa058c270ffdee14f05c9f
   useEffect(() => {
     dispatch(setPageTitle({ title: "Vendor Payout Reports" }))
   }, [])
 
   return (
-<<<<<<< HEAD
     <VendorPayoutsComponent />
 
-=======
-    <>{AllowedRoles.includes(context.role) && <VendorPayoutComponent />} </>
->>>>>>> a3e5167482190782a5fa058c270ffdee14f05c9f
   )
 }
 
